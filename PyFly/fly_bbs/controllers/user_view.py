a67346309b7dde913f7139b773ee8fd40b478df9
余漪 <vhzix24@gmail.com>
2019-09-26 15:54:16 +0800

    修改user_view视图函数

--- PyFly/fly_bbs/controllers/user_view.py
+++ PyFly/fly_bbs/controllers/user_view.py
@@ -6,24 +6,33 @@ from flask import Blueprint, render_template
 
 user_view = Blueprint('user', __name__)
 
-@user_view.route('/')
-def home():
-    users = mongo.db.users.find()
-    print(type(users))
-    l = [i for i in users]
-    print(l)
-    return json.dumps(l, cls=JSONEncoder)
+# @user_view.route('/')
+# def home():
+#     users = mongo.db.users.find()
+#     print(type(users))
+#     l = [i for i in users]
+#     print(l)
+#     return json.dumps(l, cls=JSONEncoder)
 
-@user_view.route('/index/')
+@user_view.route('/')
 def index():
     return render_template('base.html')
 
 
-class JSONEncoder(json.JSONEncoder):
-    def default(self, o):
-        if isinstance(o, ObjectId):
-            return str(o)
-        elif isinstance(o, datetime.datetime):
+@user_view.route('/login/')
+def login():
+    return render_template('user/login.html')
 
-            return o.isoformat()
-        return json.JSONEncoder.default(self, o)
+@user_view.route('/register/')
+def register():
+    return render_template('user/reg.html')
+
+
+# class JSONEncoder(json.JSONEncoder):
+#     def default(self, o):
+#         if isinstance(o, ObjectId):
+#             return str(o)
+#         elif isinstance(o, datetime.datetime):
+
+#             return o.isoformat()
+#         return json.JSONEncoder.default(self, o)
